8408bd13f34e421d187a9e4d331762f92fbe0bed
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@computas.com>
2022-10-04 19:34:55 +0200

    chore: terraform fmt -recursive .

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 locals {
-  name = var.override_name == null ? "${var.system_name}-${lower(var.environment)}-kv" : var.override_name
+  name     = var.override_name == null ? "${var.system_name}-${lower(var.environment)}-kv" : var.override_name
   location = var.override_location == null ? var.resource_group.location : var.override_location
 }
 
