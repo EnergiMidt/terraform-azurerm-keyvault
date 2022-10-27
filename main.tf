d953d6e2baa163fe160a6eefd0a2b0f076ab43f1
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@computas.com>
2022-10-27 14:55:02 +0200

    fix: contact argument block

--- main.tf
+++ main.tf
@@ -18,11 +18,8 @@ resource "azurerm_key_vault" "key_vault" {
       object_id = try(var.configuration.access_policy.object_id, null)
 
       certificate_permissions = try(var.configuration.access_policy.certificate_permissions, null)
-
       key_permissions = try(var.configuration.access_policy.key_permissions, null)
-
       secret_permissions = try(var.configuration.access_policy.secret_permissions, null)
-
       storage_permissions = try(var.configuration.access_policy.storage_permissions, null)
     }
   } # (Optional) A list of up to 16 objects describing access policies, as described above.
@@ -31,6 +28,10 @@ resource "azurerm_key_vault" "key_vault" {
   enabled_for_disk_encryption     = try(var.enabled_for_disk_encryption, null)
   enabled_for_template_deployment = try(var.enabled_for_template_deployment, null)
   enable_rbac_authorization       = try(var.enable_rbac_authorization, null)
+
+  # checkov:skip=CKV_AZURE_109: The `default_action` variable defaults to Allow.
+  # https://docs.bridgecrew.io/docs/ensure-azure-cosmosdb-has-local-authentication-disabled
+  # tfsec:ignore:azure-keyvault-specify-network-acl
 
   dynamic "network_acls" {
     for_each = try(var.configuration.network_acls, null) != null ? [var.configuration.network_acls] : []
@@ -42,16 +43,6 @@ resource "azurerm_key_vault" "key_vault" {
     }
   }
 
-  # network_acls {
-  #   bypass = var.network_acls_bypass
-  #   # checkov:skip=CKV_AZURE_109: The `default_action` variable defaults to Allow.
-  #   # https://docs.bridgecrew.io/docs/ensure-azure-cosmosdb-has-local-authentication-disabled
-  #   # tfsec:ignore:azure-keyvault-specify-network-acl
-  #   default_action             = var.network_acls_default_action
-  #   ip_rules                   = var.network_acls_ip_rules
-  #   virtual_network_subnet_ids = var.network_acls_virtual_network_subnet_ids
-  # }
-
   # https://docs.bridgecrew.io/docs/ensure-the-key-vault-is-recoverable
   # checkov:skip=CKV_AZURE_42: The `purge_protection_enabled` variable defaults to false.
   # https://docs.bridgecrew.io/docs/ensure-that-key-vault-enables-purge-protection
@@ -62,6 +53,15 @@ resource "azurerm_key_vault" "key_vault" {
   purge_protection_enabled      = try(var.purge_protection_enabled, null)
   public_network_access_enabled = try(var.public_network_access_enabled, null)
   soft_delete_retention_days    = try(var.soft_delete_retention_days, null)
+
+  dynamic "contact" {
+    for_each = try(var.configuration.contact, null) != null ? [var.configuration.contact] : []
+    content {
+      email = var.configuration.email
+      name = try(var.configuration.name, null)
+      phone = try(var.configuration.phone, null)
+    }
+  }
 
   tags = try(var.tags, null)
 }
